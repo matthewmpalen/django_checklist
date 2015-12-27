905e216a7fd3e68866519d9bf47e4a0bbc188b42
Matt Palen <matthewmpalen@gmail.com>
2015-12-27 14:16:07 -0500

    Only checklist owner is allowed to change item

--- django_checklist/todo/permissions.py
+++ django_checklist/todo/permissions.py
@@ -13,9 +13,24 @@ class ChecklistPermissions(permissions.DjangoModelPermissions):
         return obj.user == request.user
 
 class ItemPermissions(permissions.DjangoModelPermissions):
+    def has_permission(self, request, view):
+        validated = super().has_permission(request, view)
+        if not validated:
+            return False
+
+        if view.action == 'create':
+            checklist_url = request.data['checklist']
+            tokens = checklist_url.split('/')
+            checklist_id = int(tokens[-2])
+            checklist = Checklist.objects.get(pk=checklist_id)
+            if checklist.user != request.user:
+                return False
+
+        return True
+
     def has_object_permission(self, request, view, obj):
         validated = super().has_object_permission(request, view, obj)
         if not validated:
             return False
 
-        return obj.user == request.user
+        return obj.checklist.user == request.user
